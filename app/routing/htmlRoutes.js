ae405061fd1295143a9e2a447892aafd57cc1786
karlosgardner <karlosgardner@comcast.net>
2017-04-29 05:42:22 -0400

    image

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,7 +5,9 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
-
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/loving_black_couple.jpg"));
+  });
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
